a379c4f8ec8a7cb221e404465c7f2ec45d4fbfa8
Ainsley <aidan.king1999@hotmil.co.uk>
2014-03-03 12:04:35 +0000

    Create power.py

---
+++ power.py
@@ -0,0 +1,5 @@
+firstLine = "And ohhh, I'm alive, I'm alive, I'm alive."
+secondLine = "And I'm loving every second, minute, hour, bigger, better, stronger, that power."
+thirdLine = "Bum bum bum ba bum, bum, ba bum bum ba ba bum."
+sentance = firstLine + secondLine + thirdLine
+print(sentance)
